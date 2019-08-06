a1a95cbead0dbffe3122e1d63f761bd69983a2c4
三宅皐 <lazymii@iMac.local>
2019-08-07 00:54:52 +0900

    use lombok

--- src/main/java/com/example/demo/domain/Task.java
+++ src/main/java/com/example/demo/domain/Task.java
@@ -1,42 +1,20 @@
 package com.example.demo.domain;
 
+import lombok.Data;
+
 import java.io.Serializable;
 
 import javax.persistence.*;
 
+@Data
 @Entity
 @Table(name = "task")
 public class Task implements Serializable {
     private static final long serialVersionUID = 1L;
 
-
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
-    public Integer id;
-    public String title;
-    public String description;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId() {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
+    private Integer id;
+    private String title;
+    private String description;
 }
